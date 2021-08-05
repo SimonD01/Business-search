0fa3b9f50bc95b3656cce1d29f6082005c436ded
Simon Denisov <simondnsv@gmail.com>
2021-08-04 18:06:30 -0700

    best oc cafe search

--- search.py
+++ search.py
@@ -1,1 +1,20 @@
+import requests
+import config
 
+url = "https://api.yelp.com/v3/businesses/search"
+
+headers = {
+    "Authorization": "Bearer " + config.api_key
+}
+params = {
+    "terms": "cafe",
+    "location": "OC"
+}
+response = requests.get(url, headers=headers, params=params)
+businesses = response.json()["businesses"]
+for business in businesses:
+    business["name"]
+
+names = [business["name"]
+         for business in businesses if business["rating"] > 0]
+print(names)
